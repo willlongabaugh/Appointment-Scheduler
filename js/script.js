7774d1b41a9f07849bef83d252beeeaafcccfcd5
Marie Margishvili <mmargishvili15@amherst.edu>
2013-11-13 15:54:48 -0500

    starting to add back-end (firebase)

---
+++ js/script.js
@@ -0,0 +1,132 @@
+
+
+
+$(document).ready(function() {
+	var divLexie;
+  var dropDown=$( "#dropdown option:selected" ).val();
+
+  if(dropDown == "default"){
+    $('#calendar-container').hide();
+  }
+  $(document).on('change','#dropdown',function(e){
+    if($('#dropdown option:selected' ).val() != "default"){
+      $('#calendar-container').show();}
+      else $('#calendar-container').hide();
+    });
+  $('div').click(function() {
+
+    if($(this).hasClass("available")) {
+     divLexie=$(this);
+     $( "#dialog-form" ).dialog( "open" );
+     $(this).attr("class", "half-hour taken");
+     $(this).text("Taken");
+   } else if($(this).hasClass("taken")) {
+     $(this).attr("class", "half-hour available");
+     $(this).text("Available");
+   }
+ });
+
+
+  $(function() {
+    var name = $( "#name" ),
+    email = $( "#email" ),
+    password = $( "#password" ),
+    allFields = $( [] ).add( name ).add( email ).add( password ),
+    tips = $( ".validateTips" );
+
+    function updateTips( t ) {
+      tips
+      .text( t )
+      .addClass( "ui-state-highlight" );
+      setTimeout(function() {
+        tips.removeClass( "ui-state-highlight", 1500 );
+      }, 500 );
+    }
+    
+    
+    
+    
+    $(function() {
+      var name = $( "#name" ),
+      email = $( "#email" ),
+      reason = $( "#reason" ),
+      allFields = $( [] ).add( name ).add( email ).add( reason ),
+      tips = $( ".validateTips" );
+
+      function updateTips( t ) {
+        tips
+        .text( t )
+        .addClass( "ui-state-highlight" );
+        setTimeout(function() {
+          tips.removeClass( "ui-state-highlight", 1500 );
+        }, 500 );
+      }
+
+      function checkLength( o, n, min, max ) {
+        if ( o.val().length > max || o.val().length < min ) {
+          o.addClass( "ui-state-error" );
+          updateTips( "Length of " + n + " must be between " +
+            min + " and " + max + "." );
+          return false;
+        } else {
+          return true;
+        }
+      }
+
+      function checkRegexp( o, regexp, n ) {
+        if ( !( regexp.test( o.val() ) ) ) {
+          o.addClass( "ui-state-error" );
+          updateTips( n );
+          return false;
+        } else {
+          return true;
+        }
+      }
+
+      $( "#dialog-form" ).dialog({
+        autoOpen: false,
+        dialogClass: "no-close",
+        height: 420,
+        width: 400,
+        modal: true,
+        buttons: {
+          "Create an appointment": function() {
+            var bValid = true;
+            allFields.removeClass( "ui-state-error" );
+
+            bValid = bValid && checkLength( name, "username", 3, 26 );
+            bValid = bValid && checkLength( email, "email", 6, 80 );
+            bValid = bValid && checkLength( reason, "reason", 3, 300 );
+
+            bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+          // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+          bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+       //   bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+
+       if ( bValid ) {
+
+        $(divLexie).append( "<tr>" +
+          "<td>" + name.val() + "</td>" +
+          "<td>" + email.val() + "</td>" +
+          "<td>" + reason.val() + "</td>" +
+          "</tr>" );
+        $( this ).dialog( "close" );
+      }
+    },
+    Cancel: function() {
+      $(divLexie).attr("class", "half-hour available");
+      $(divLexie).text("Available");
+      $( this ).dialog( "close" );
+    }
+  },
+  close: function() {
+    allFields.val( "" ).removeClass( "ui-state-error" );
+  }
+});
+
+});
+});
+
+
+
+});
